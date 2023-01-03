0b3524e2fe59bc5d57e6f693958810f99ce8c7ad
Dirga <115365051+Holylight5@users.noreply.github.com>
2023-01-04 05:15:40 +0700

    Add files via upload

---
+++ pbo9/Buah.java
@@ -0,0 +1,24 @@
+
+package pbo9;
+
+import java.util.ArrayList;
+
+public class Buah {
+
+    public static void main(String[] args) {
+    String[] Buah = new String[3];
+    Buah[0] = "jeruk";
+    Buah[1] = "Pisang";
+    Buah[2] = "Semangka";
+    
+    System.out.println(Buah[1]);
+        
+    ArrayList data_buah = new ArrayList();
+    data_buah.add("jeruk");
+    data_buah.add("Pisang");
+    data_buah.add("Saemangka");
+    data_buah.add("Papaya");
+        System.out.println(data_buah);
+    }
+    
+}
